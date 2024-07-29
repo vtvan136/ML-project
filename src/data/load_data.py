f2f82b32caa381d1b4b05d14bc2ae4b68aeb8262
vtvan <vtvan136@gmail.com>
2024-07-29 13:30:07 +0700

    update load_data & README file

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,1 +1,8 @@
 # Dùng để load data
+from torchvision import datasets, models, transforms
+from torchvision.datasets import ImageFolder
+data_dir = "data/processed"
+train_dir = data_dir + "/train"
+valid_dir = data_dir + "/valid"
+train = ImageFolder(train_dir, transform=transforms.ToTensor())
+valid = ImageFolder(valid_dir, transform=transforms.ToTensor())
